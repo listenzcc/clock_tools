0744e6a8008a2b145e8ac681991621ddbbca22aa
zcc <listenzcc@mail.bnu.edu.cn>
2018-11-13 16:29:42 +0800

    add Timer name and write comments, complex test

--- simple_timer.py
+++ simple_timer.py
@@ -6,39 +6,52 @@ import threading
 
 class simple_timer:
     # A simple timer tool
-    def __init__(self):
+    def __init__(self, name='Timer'):
+        # happy birthday
         self.begin = time.time()
+        # name myself
+        self.name = name
+        # set empty threads for alarmers
         self.threads = []
 
     def status(self):
-        print('[Timer status]')
+        # print status of self
+        print('[%s status]' % self.name)
         print('\tBegin at ' + time.ctime(self.begin))
         self.refresh_threads()
         print('\t%d alarms forward' % len(self.threads))
         self.click()
 
     def refresh_threads(self):
+        # refresh threads, discharging outdate alamer
         self.threads = list(e for e in self.threads if e.is_alive())
 
     def refresh(self):
+        # refresh threads and reset birthday
         self.refresh_threads()
         self.begin = time.time()
 
     def start(self):
+        # false start function ^_^
         self.refresh()
 
     def alarm_after(self, delay, todo='Something'):
+        # setup an alarmer
         t = threading.Thread(
             target=self.alarm_after_thread, args=(delay, todo))
+        # stack into threads
         self.threads.append(t)
+        # running as new threading
         t.start()
 
     def alarm_after_thread(self, delay, todo):
+        # alarmer function, delay and report
         time.sleep(delay)
-        print('[Timer alarm]')
+        print('[%s alarm]' % self.name)
         print('\t%d seconds passed, %s'
               % (delay, todo))
 
     def click(self, message='passed.'):
-        print('[Timer click]')
+        # report current time on click
+        print('[%s click]' % self.name)
         print('\t%0.2f seconds %s' % (time.time() - self.begin, message))
--- test.py
+++ test.py
@@ -5,9 +5,12 @@ import time
 
 if __name__ == '__main__':
     print('hello world')
-    st = simple_timer()
+    st = simple_timer('Tim')
+    st1 = simple_timer('AA')
     st.status()
     st.alarm_after(2, '2 seconds alarm')
+    st1.alarm_after(10, '10 seconds alarm')
+    st1.status()
     st.alarm_after(5, '5 seconds alarm')
     time.sleep(3)
     st.status()
